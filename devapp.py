67eb65b975788e3e7ee119ab6aea73db33e4c3de
Mabel123-blip <mabelbabatola0@gmail.com>
2025-07-09 04:08:32 -0700

    Update devapp.py to reflect changes

--- devapp.py
+++ devapp.py
@@ -26,4 +26,5 @@ def predict():
 
 if __name__ == '__main__':
     print("Starting Flask app...")
-    app.run(host='0.0.0.0', port=port,debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug = True)
